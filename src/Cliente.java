3b1aa80d8a00d10454186c6a3cbcfeadc63b7510
Felipe Reis <felipereisdaniel@hotmail.com>
2024-04-12 22:59:04 -0300

    adicionando cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,16 @@
+
+
+
 public class Cliente {
+
+    private String nome;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
     
+    public String getNome(){
+        return nome;
+    }
 }
