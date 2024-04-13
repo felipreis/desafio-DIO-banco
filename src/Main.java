3a2133cf8c34a7a9851e14777dec7ab6875503fc
Felipe Reis <felipereisdaniel@hotmail.com>
2024-04-12 23:26:51 -0300

    adicionando classe main

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Cliente clienteUm = new Cliente();
+        Cliente clienteDois = new Cliente();
+        clienteUm.setNome("Felipe");
+        clienteDois.setNome("Mariana");
+
+        Conta contaCorrente = new ContaCorrente(clienteUm);
+        Conta contaPoupanca = new ContaPoupanca(clienteDois);
+
+        contaCorrente.depositar(200.00);
+        contaCorrente.transferir(145.50, contaPoupanca);
+
+        contaPoupanca.sacar(75.00);
+
+        contaCorrente.exibirExtrato();
+        contaPoupanca.exibirExtrato();
+
+
+
+    }
+
+
+}
